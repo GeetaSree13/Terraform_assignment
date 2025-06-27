de8baf1cfbb141fb2fd5b872a2476fbfc6cb1349
penumarthigeeta <penumarthi.sree@gdn-commerce.com>
2025-06-27 11:07:30 +0530

    added change

--- basics/main.tf
+++ basics/main.tf
@@ -6,7 +6,7 @@ terraform {
 }
 
 resource "google_compute_instance" "vm_instance" {
-    name ="terraform-practice"
+    name ="terraform-practices"
     machine_type = "e2-medium"
     zone = "asia-southeast1-a"
 
@@ -45,7 +45,7 @@ resource "google_compute_firewall" "allow-https" {
     network = "default"
 
     allow {
-      protocol = "tcp"
+      protocol = "udp"
       ports = ["443"]
     }
   
