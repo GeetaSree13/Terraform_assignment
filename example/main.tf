f98292fce4d47b580180015358c6e4c66182cf39
penumarthigeeta <penumarthi.sree@gdn-commerce.com>
2025-06-27 12:54:22 +0530

    added

--- example/main.tf
+++ example/main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_instance" "vm_instance" {
-    name ="terraform-practices"
+    name ="terraform-practicess"
     machine_type = "e2-medium"
     zone = "asia-southeast1-a"
 
