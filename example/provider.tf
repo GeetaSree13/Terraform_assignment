8dd92fd7ccfe4d1a44d875c7fe344de28017a4ef
penumarthigeeta <penumarthi.sree@gdn-commerce.com>
2025-06-27 12:46:48 +0530

    added

--- example/provider.tf
+++ example/provider.tf
@@ -2,4 +2,5 @@ provider "google" {
     project = "tonal-benefit-462606-b8"
     region = "asia-southeast1"
     zone   = "asia-southeast1-a"
+    credentials = file("/home/atlantis/.atlantis/repos/GeetaSree13/Terraform_assignment/creds.json")
 }
